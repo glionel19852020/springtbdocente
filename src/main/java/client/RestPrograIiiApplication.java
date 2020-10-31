cf0b11a6a27b825b94d7ae924cbbfb837ce47726
gerson <gersonleoneln@hotmail.com>
2020-10-30 18:43:42 -0600

    cambios finales

--- src/main/java/client/RestPrograIiiApplication.java
+++ src/main/java/client/RestPrograIiiApplication.java
@@ -2,8 +2,10 @@ package client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class RestPrograIiiApplication {
 
 	public static void main(String[] args) {
