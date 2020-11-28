9a7be375d12ef522a8aa926c68fc710ef009b535
gerson <gersonleoneln@hotmail.com>
2020-11-27 20:40:42 -0600

    aplicación de seguridad basica (Basic security)

---
+++ src/main/java/client/seguridad/BasicAuthenticationController.java
@@ -0,0 +1,15 @@
+package client.seguridad;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins={ "http://localhost:8090" })
+@RestController
+public class BasicAuthenticationController {
+	@GetMapping(path = "/basicauth")
+    public AuthenticationBean helloWorldBean() {
+        return new AuthenticationBean("Te has autenticado exitosamente");
+    }   
+
+}
